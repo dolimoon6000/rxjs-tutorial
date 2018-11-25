0c177f6e312d761d4e867f5508d51158f310156b
Olu Soboyede <dolapo@yinkos.com>
2018-11-25 11:09:56 +0000

    Creating observable from a click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { interval, of } from 'rxjs';
-import { map, take, switchMap } from 'rxjs/internal/operators';
+import { fromEvent } from 'rxjs';
 
 
 @Component({
@@ -13,12 +12,7 @@ export class AppComponent {
   mySubject$;
 
   ngOnInit() {
-    const numbers$ = interval(1000);
-    const letters$ = of('a', 'b', 'c', 'd', 'e');
-
-    letters$.pipe(switchMap(x =>
-      numbers$.pipe(take(5)).pipe(map(i => i + x))
-    )).subscribe(x => console.log(x));
+    fromEvent(document, 'click').subscribe(x => console.log(x));
   }
 
   ngOnDestroy() {
