393dad4f64bae19c8f58005d9463d237f248c098
Olu Soboyede <dolapo@yinkos.com>
2018-11-25 10:25:49 +0000

    Create a new observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+  observable$;
+
+  ngOnInit() {
+    const observable$ = Observable.create(observer => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+
+
+    observable$.subscribe(
+      value => console.log(value),
+      err => {},
+      () => console.log('this is the end')
+    );
+  }
+
+  ngOnDestroy() {
+    this.observable$.unsubscribe();
+  }
 }
